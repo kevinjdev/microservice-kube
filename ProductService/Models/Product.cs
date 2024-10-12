6380aebb1f6de3d97700a068ea3d98cf17113adf
Kevin Jaeger <kevin.a.jaeger@gmail.com>
2024-10-12 07:45:52 -0600

    initial commit

---
+++ ProductService/Models/Product.cs
@@ -0,0 +1,6 @@
+public class Product
+{
+    public required int Id { get; set; }
+    public required string Name { get; set; }
+    public required string Price { get; set; }
+}
---
+++ ProductService/Program.cs
@@ -0,0 +1,70 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSingleton<List<Product>>();
+
+var app = builder.Build();
+
+SeedData(app.Services.GetRequiredService<List<Product>>());
+
+// Configure the API endpoints.
+app.MapGet("/api/products", (List<Product> products) =>
+{
+    return Results.Ok(products);
+});
+
+app.MapGet("/api/products/{id}", (int id, List<Product> products) =>
+{
+    var product = products.FirstOrDefault(p => p.Id == id);
+    if (product == null)
+    {
+        return Results.NotFound();
+    }
+    return Results.Ok(product);
+});
+
+app.MapPost("/api/products", (Product product, List<Product> products) =>
+{
+    products.Add(product);
+    return Results.Created($"/api/products/{product.Id}", product);
+});
+
+app.MapPut("/api/products/{id}", (int id, Product updatedProduct, List<Product> products) =>
+{
+    var product = products.FirstOrDefault(p => p.Id == id);
+    if (product == null)
+    {
+        return Results.NotFound();
+    }
+
+    product.Name = updatedProduct.Name;
+    product.Price = updatedProduct.Price;
+
+    return Results.NoContent();
+});
+
+app.MapDelete("/api/products/{id}", (int id, List<Product> products) =>
+{
+    var product = products.FirstOrDefault(p => p.Id == id);
+    if (product == null)
+    {
+        return Results.NotFound();
+    }
+
+    products.Remove(product);
+    return Results.NoContent();
+});
+
+app.Run();
+
+static void SeedData(List<Product> products)
+{
+        products.AddRange(new List<Product>
+    {
+        new Product { Id = 1, Name = "Product A", Price = "$1.99" },
+        new Product { Id = 2, Name = "Product B", Price = "$2.99" },
+        new Product { Id = 3, Name = "Product C", Price = "$3.99" },
+        new Product { Id = 4, Name = "Product D", Price = "$4.99" },
+        new Product { Id = 5, Name = "Product E", Price = "$5.99" },
+        new Product { Id = 6, Name = "Product F", Price = "$6.99" },
+    });
+}
